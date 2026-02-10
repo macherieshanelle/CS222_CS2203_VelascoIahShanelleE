51d85657810987395432cc9973038aa759bd9891
Shanelle <shanellevelasco6@yahoo.com>
2026-02-10 16:19:25 +0800

    Add files via upload

---
+++ CS222_CS2203_VelascoIahShanelleE/Practice Assessment 1/CurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class CurrencyConverter
+{
+    static void Main()
+    {
+        Console.Write("Enter amount in USD: ");
+        double USD = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter exchange rate from USD to EUR: ");
+        double exchangeRate = Convert.ToDouble(Console.ReadLine());
+
+        double EUR = USD * exchangeRate;
+
+        Console.WriteLine("Amount in EUR: " + EUR.ToString("F2"));
+    }
+}
+
